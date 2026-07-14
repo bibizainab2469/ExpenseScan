b5a33e8f32bd0ffff8924c11bb1f6dfe1d0d6a23
Bibi Zainab <bibizainab2469@gmail.com>
2026-07-14 15:10:30 +0500

    "Multiple entries working"

--- main.py
+++ main.py
@@ -3,17 +3,29 @@ import chromadb
 client = chromadb.Client()
 collection = client.create_collection("expenses")
 
-# Step 1: Take input
-expense = input("Enter your expense: ")
+# Store multiple expenses
+expenses = []
 
-# Step 2: Store it
-collection.add(
-    documents=[expense],
-    ids=["1"]
-)
-print("Stored!")
-
-# Step 3: Query
-query = input("Ask about your expenses: ")
-results = collection.query(query_texts=[query], n_results=1)
-print(results)
+while True:
+    print("\n1. Add expense")
+    print("2. Query expenses")
+    print("3. Exit")
+    
+    choice = input("Choose: ")
+    
+    if choice == "1":
+        expense = input("Enter expense: ")
+        id = str(len(expenses) + 1)
+        collection.add(documents=[expense], ids=[id])
+        expenses.append(expense)
+        print("Stored!")
+    
+    elif choice == "2":
+        query = input("Ask: ")
+        results = collection.query(query_texts=[query], n_results=3)
+        print("\nResults:")
+        for doc in results['documents'][0]:
+            print("-", doc)
+    
+    elif choice == "3":
+        break
